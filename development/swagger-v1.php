7955c4e29ba06f9ff3c1fdcec0d07e064b5de12a
Tai Nguyen <nguyenductai24081996@gmail.com>
2023-06-20 14:02:38 +0700

    update swagger config

---
+++ development/swagger-v1.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @SWG\Swagger(
+ *     schemes={"https"},
+ *     host=API_HOST,
+ *     basePath="/",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="ibclab API Docs",
+ *         description="API Docs",
+ *         termsOfService="",
+ *         @SWG\Contact(
+ *             email="nguyenductai24081996@gmail.com"
+ *         ),
+ *     ),
+ *     @SWG\SecurityScheme(
+ *         securityDefinition="bearerAuth",
+ *         type="apiKey",
+ *         name="Authorization",
+ *         in="header",
+ *     ),
+ * )
+ */
+
