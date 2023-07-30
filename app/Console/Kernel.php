0cce63419b1c65cb6d5460d450211063fab0e677
Tai Nguyen <nguyenductai24081996@gmail.com>
2023-07-30 19:24:17 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('summary:commit')->hourly();
+         $schedule->command('summary:commit')->hourlyAt(30);
     }
 
     /**
