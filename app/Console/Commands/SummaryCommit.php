4bdedc1b471523c0ffeb9231e9eba8ebe0d38456
Tai Nguyen <nguyenductai24081996@gmail.com>
2024-01-22 02:13:18 +0700

    fix

--- app/Console/Commands/SummaryCommit.php
+++ app/Console/Commands/SummaryCommit.php
@@ -49,8 +49,6 @@ class SummaryCommit extends Command
         $key = 1;
         $sha = CommitSHA::orderBy("id", "ASC")->get();
         foreach ($sha as $item){
-            setting()->set("sha_id", $item->id);
-            setting()->save();
             $commit = Commit::whereId($item->commit_id)->first();
             $prefix = $commit->target_repo->github_prefix;
             $detailUrl = "https://api.github.com/repos/$prefix/commits/" . $item->sha;
@@ -58,8 +56,7 @@ class SummaryCommit extends Command
             if (isset($detail["message"])){
                 Log::error($detail["message"]);
                 if (strpos($detail["message"], "API rate limit") !== false) {
-                    $key = ($key == 1) ? 2 : 1;
-                    continue;
+                    return 1;
                 }
                 if (strpos($detail["message"], "Not") !== false) {
                     $item->delete();
